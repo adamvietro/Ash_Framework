e343ad9077e2266beae1547e9ab40149caa5c201
adam vietro <hunterafi@gmail.com>
2025-09-26 16:51:22 -0700

    Ash Extend Json_API Set the new domains and resources for the Music domain. Added in some new routes to the router.

--- tunez/lib/tunez/music/artist.ex
+++ tunez/lib/tunez/music/artist.ex
@@ -1,59 +1,66 @@
 defmodule Tunez.Music.Artist do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshJsonApi.Resource]
+
+  json_api do
+    type "artist"
+  end
 
   postgres do
-    table("artists")
-    repo(Tunez.Repo)
+    table "artists"
+    repo Tunez.Repo
 
     custom_indexes do
-      index("name gin_trgm_ops", name: "artists_name_trgm_index", using: "GIN")
+      index "name gin_trgm_ops", name: "artists_name_trgm_index", using: "GIN"
+    end
+  end
+
+  actions do
+    defaults [:create, :read, :destroy]
+    default_accept [:name, :biography]
+
+    update :update do
+      require_atomic? false
+      accept [:name, :biography]
+
+      change Tunez.Music.Changes.UpdatePreviousNames,
+        where: [changing(:name)]
+    end
+
+    read :search do
+      argument :query, :ci_string do
+        constraints allow_empty?: true
+        default ""
+      end
+
+      filter expr(contains(name, ^arg(:query)))
+      pagination offset?: true, default_limit: 12
     end
   end
 
   attributes do
-    uuid_primary_key(:id)
+    uuid_primary_key :id
 
     attribute :name, :string do
-      allow_nil?(false)
-      public?(true)
+      allow_nil? false
+      public? true
     end
 
     attribute :previous_names, {:array, :string} do
-      default([])
+      default []
     end
 
-    attribute(:biography, :string)
-    create_timestamp(:inserted_at, public?: true)
-    update_timestamp(:updated_at, public?: true)
-  end
-
-  actions do
-    defaults([:create, :read, :destroy])
-    default_accept([:name, :biography])
-
-    update :update do
-      require_atomic?(false)
-      accept([:name, :biography])
-
-      change(Tunez.Music.Changes.UpdatePreviousNames,
-        where: [changing(:name)]
-      )
-    end
-
-    read :search do
-      argument(:query, :ci_string) do
-        constraints(allow_empty?: true)
-        default("")
-      end
-
-      filter(expr(contains(name, ^arg(:query))))
-      pagination(offset?: true, default_limit: 12)
-    end
+    attribute :biography, :string
+    create_timestamp :inserted_at, public?: true
+    update_timestamp :updated_at, public?: true
   end
 
   relationships do
     has_many :albums, Tunez.Music.Album do
-      sort(year_released: :desc)
+      sort year_released: :desc
     end
   end
 
@@ -65,18 +72,18 @@ defmodule Tunez.Music.Artist do
 
   aggregates do
     # calculate :album_count, :integer, expr(count(albums))
-    count(:album_count, :albums) do
-      public?(true)
+    count :album_count, :albums do
+      public? true
     end
 
     # calculate :latest_album_year_released, :integer,
     # expr(first(albums, field: :year_released))
-    first(:latest_album_year_released, :albums, :year_released) do
-      public?(true)
+    first :latest_album_year_released, :albums, :year_released do
+      public? true
     end
 
     # calculate :cover_image_url, :string,
     # expr(first(albums, field: :cover_image_url))
-    first(:cover_image_url, :albums, :cover_image_url)
+    first :cover_image_url, :albums, :cover_image_url
   end
 end
