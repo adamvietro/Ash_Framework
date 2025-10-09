5c501f928faff05a116086555ba637065eb901fd
adam vietro <hunterafi@gmail.com>
2025-10-09 12:16:57 -0700

    Grahpql Set up the grapgql framework. Needed to use mutations and queries as well as define the proper response from the server.

--- tunez/lib/tunez/accounts.ex
+++ tunez/lib/tunez/accounts.ex
@@ -1,22 +1,36 @@
 defmodule Tunez.Accounts do
-  use Ash.Domain, otp_app: :tunez, extensions: [AshJsonApi.Domain]
+  use Ash.Domain, otp_app: :tunez, extensions: [AshGraphql.Domain, AshJsonApi.Domain]
+
+  graphql do
+    mutations do
+      create(Tunez.Accounts.User, :register_user, :register_with_password)
+    end
+
+    queries do
+      get(Tunez.Accounts.User, :sign_in_user, :sign_in_with_password) do
+        type_name :user_with_token
+      end
+    end
+  end
 
   json_api do
-    base_route "/users", Tunez.Accounts.User do
-      post :register_with_password do
-        route("/register")
+    routes do
+      base_route "/users", Tunez.Accounts.User do
+        post :register_with_password do
+          route("/register")
 
-        metadata(fn _subject, user, _request ->
-          %{token: user.__metadata__.token}
-        end)
-      end
+          metadata(fn _subject, user, _request ->
+            %{token: user.__metadata__.token}
+          end)
+        end
 
-      post :sign_in_with_password do
-        route("/sign_in")
+        post :sign_in_with_password do
+          route("/sign_in")
 
-        metadata(fn _subject, user, _request ->
-          %{token: user.__metadata__.token}
-        end)
+          metadata(fn _subject, user, _request ->
+            %{token: user.__metadata__.token}
+          end)
+        end
       end
     end
   end
--- tunez/lib/tunez/accounts/user.ex
+++ tunez/lib/tunez/accounts/user.ex
@@ -4,122 +4,123 @@ defmodule Tunez.Accounts.User do
     domain: Tunez.Accounts,
     data_layer: AshPostgres.DataLayer,
     authorizers: [Ash.Policy.Authorizer],
-    extensions: [AshJsonApi.Resource, AshAuthentication]
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource, AshAuthentication]
 
   authentication do
     add_ons do
       log_out_everywhere do
-        apply_on_password_change?(true)
+        apply_on_password_change? true
       end
 
       confirmation :confirm_new_user do
-        monitor_fields([:email])
-        confirm_on_create?(true)
-        confirm_on_update?(false)
-        require_interaction?(true)
-        confirmed_at_field(:confirmed_at)
-        auto_confirm_actions([:sign_in_with_magic_link, :reset_password_with_token])
-        sender(Tunez.Accounts.User.Senders.SendNewUserConfirmationEmail)
+        monitor_fields [:email]
+        confirm_on_create? true
+        confirm_on_update? false
+        require_interaction? true
+        confirmed_at_field :confirmed_at
+        auto_confirm_actions [:sign_in_with_magic_link, :reset_password_with_token]
+        sender Tunez.Accounts.User.Senders.SendNewUserConfirmationEmail
       end
     end
 
     tokens do
-      enabled?(true)
-      token_resource(Tunez.Accounts.Token)
-      signing_secret(Tunez.Secrets)
-      store_all_tokens?(true)
-      require_token_presence_for_authentication?(true)
+      enabled? true
+      token_resource Tunez.Accounts.Token
+      signing_secret Tunez.Secrets
+      store_all_tokens? true
+      require_token_presence_for_authentication? true
     end
 
     strategies do
       password :password do
-        identity_field(:email)
-        hash_provider(AshAuthentication.BcryptProvider)
+        identity_field :email
+        hash_provider AshAuthentication.BcryptProvider
 
         resettable do
-          sender(Tunez.Accounts.User.Senders.SendPasswordResetEmail)
+          sender Tunez.Accounts.User.Senders.SendPasswordResetEmail
           # these configurations will be the default in a future release
-          password_reset_action_name(:reset_password_with_token)
-          request_password_reset_action_name(:request_password_reset_token)
+          password_reset_action_name :reset_password_with_token
+          request_password_reset_action_name :request_password_reset_token
         end
       end
 
       magic_link do
-        identity_field(:email)
-        registration_enabled?(true)
-        require_interaction?(true)
+        identity_field :email
+        registration_enabled? true
+        require_interaction? true
 
-        sender(Tunez.Accounts.User.Senders.SendMagicLinkEmail)
+        sender Tunez.Accounts.User.Senders.SendMagicLinkEmail
       end
     end
   end
 
+  graphql do
+    type :user
+  end
+
   json_api do
-    type("user")
+    type "user"
   end
 
   postgres do
-    table("users")
-    repo(Tunez.Repo)
+    table "users"
+    repo Tunez.Repo
   end
 
   actions do
-    defaults([:read])
+    defaults [:read]
 
     read :get_by_subject do
-      description("Get a user by the subject claim in a JWT")
-      argument(:subject, :string, allow_nil?: false)
-      get?(true)
-      prepare(AshAuthentication.Preparations.FilterBySubject)
+      description "Get a user by the subject claim in a JWT"
+      argument :subject, :string, allow_nil?: false
+      get? true
+      prepare AshAuthentication.Preparations.FilterBySubject
     end
 
     update :change_password do
       # Use this action to allow users to change their password by providing
       # their current password and a new password.
 
-      require_atomic?(false)
-      accept([])
-      argument(:current_password, :string, sensitive?: true, allow_nil?: false)
+      require_atomic? false
+      accept []
+      argument :current_password, :string, sensitive?: true, allow_nil?: false
 
-      argument(:password, :string,
+      argument :password, :string,
         sensitive?: true,
         allow_nil?: false,
         constraints: [min_length: 8]
-      )
 
-      argument(:password_confirmation, :string, sensitive?: true, allow_nil?: false)
+      argument :password_confirmation, :string, sensitive?: true, allow_nil?: false
 
-      validate(confirm(:password, :password_confirmation))
+      validate confirm(:password, :password_confirmation)
 
-      validate(
-        {AshAuthentication.Strategy.Password.PasswordValidation,
-         strategy_name: :password, password_argument: :current_password}
-      )
+      validate {AshAuthentication.Strategy.Password.PasswordValidation,
+                strategy_name: :password, password_argument: :current_password}
 
-      change({AshAuthentication.Strategy.Password.HashPasswordChange, strategy_name: :password})
+      change {AshAuthentication.Strategy.Password.HashPasswordChange, strategy_name: :password}
     end
 
     read :sign_in_with_password do
-      description("Attempt to sign in using a email and password.")
-      get?(true)
+      description "Attempt to sign in using a email and password."
+      get? true
 
       argument :email, :ci_string do
-        description("The email to use for retrieving the user.")
-        allow_nil?(false)
+        description "The email to use for retrieving the user."
+        allow_nil? false
       end
 
       argument :password, :string do
-        description("The password to check for the matching user.")
-        allow_nil?(false)
-        sensitive?(true)
+        description "The password to check for the matching user."
+        allow_nil? false
+        sensitive? true
       end
 
       # validates the provided email and password and generates a token
-      prepare(AshAuthentication.Strategy.Password.SignInPreparation)
+      prepare AshAuthentication.Strategy.Password.SignInPreparation
 
       metadata :token, :string do
-        description("A JWT that can be used to authenticate the user.")
-        allow_nil?(false)
+        description "A JWT that can be used to authenticate the user."
+        allow_nil? false
       end
     end
 
@@ -132,171 +133,171 @@ defmodule Tunez.Accounts.User do
       # liveviews, you may remove this action, and set
       # `sign_in_tokens_enabled? false` in the password strategy.
 
-      description("Attempt to sign in using a short-lived sign in token.")
-      get?(true)
+      description "Attempt to sign in using a short-lived sign in token."
+      get? true
 
       argument :token, :string do
-        description("The short-lived sign in token.")
-        allow_nil?(false)
-        sensitive?(true)
+        description "The short-lived sign in token."
+        allow_nil? false
+        sensitive? true
       end
 
       # validates the provided sign in token and generates a token
-      prepare(AshAuthentication.Strategy.Password.SignInWithTokenPreparation)
+      prepare AshAuthentication.Strategy.Password.SignInWithTokenPreparation
 
       metadata :token, :string do
-        description("A JWT that can be used to authenticate the user.")
-        allow_nil?(false)
+        description "A JWT that can be used to authenticate the user."
+        allow_nil? false
       end
     end
 
     create :register_with_password do
-      description("Register a new user with a email and password.")
+      description "Register a new user with a email and password."
 
       argument :email, :ci_string do
-        allow_nil?(false)
+        allow_nil? false
       end
 
       argument :password, :string do
-        description("The proposed password for the user, in plain text.")
-        allow_nil?(false)
-        constraints(min_length: 8)
-        sensitive?(true)
+        description "The proposed password for the user, in plain text."
+        allow_nil? false
+        constraints min_length: 8
+        sensitive? true
       end
 
       argument :password_confirmation, :string do
-        description("The proposed password for the user (again), in plain text.")
-        allow_nil?(false)
-        sensitive?(true)
+        description "The proposed password for the user (again), in plain text."
+        allow_nil? false
+        sensitive? true
       end
 
       # Sets the email from the argument
-      change(set_attribute(:email, arg(:email)))
+      change set_attribute(:email, arg(:email))
 
       # Hashes the provided password
-      change(AshAuthentication.Strategy.Password.HashPasswordChange)
+      change AshAuthentication.Strategy.Password.HashPasswordChange
 
       # Generates an authentication token for the user
-      change(AshAuthentication.GenerateTokenChange)
+      change AshAuthentication.GenerateTokenChange
 
       # validates that the password matches the confirmation
-      validate(AshAuthentication.Strategy.Password.PasswordConfirmationValidation)
+      validate AshAuthentication.Strategy.Password.PasswordConfirmationValidation
 
       metadata :token, :string do
-        description("A JWT that can be used to authenticate the user.")
-        allow_nil?(false)
+        description "A JWT that can be used to authenticate the user."
+        allow_nil? false
       end
     end
 
     action :request_password_reset_token do
-      description("Send password reset instructions to a user if they exist.")
+      description "Send password reset instructions to a user if they exist."
 
       argument :email, :ci_string do
-        allow_nil?(false)
+        allow_nil? false
       end
 
       # creates a reset token and invokes the relevant senders
-      run({AshAuthentication.Strategy.Password.RequestPasswordReset, action: :get_by_email})
+      run {AshAuthentication.Strategy.Password.RequestPasswordReset, action: :get_by_email}
     end
 
     read :get_by_email do
-      description("Looks up a user by their email")
-      get?(true)
+      description "Looks up a user by their email"
+      get? true
 
       argument :email, :ci_string do
-        allow_nil?(false)
+        allow_nil? false
       end
 
-      filter(expr(email == ^arg(:email)))
+      filter expr(email == ^arg(:email))
     end
 
     update :reset_password_with_token do
       argument :reset_token, :string do
-        allow_nil?(false)
-        sensitive?(true)
+        allow_nil? false
+        sensitive? true
       end
 
       argument :password, :string do
-        description("The proposed password for the user, in plain text.")
-        allow_nil?(false)
-        constraints(min_length: 8)
-        sensitive?(true)
+        description "The proposed password for the user, in plain text."
+        allow_nil? false
+        constraints min_length: 8
+        sensitive? true
       end
 
       argument :password_confirmation, :string do
-        description("The proposed password for the user (again), in plain text.")
-        allow_nil?(false)
-        sensitive?(true)
+        description "The proposed password for the user (again), in plain text."
+        allow_nil? false
+        sensitive? true
       end
 
       # validates the provided reset token
-      validate(AshAuthentication.Strategy.Password.ResetTokenValidation)
+      validate AshAuthentication.Strategy.Password.ResetTokenValidation
 
       # validates that the password matches the confirmation
-      validate(AshAuthentication.Strategy.Password.PasswordConfirmationValidation)
+      validate AshAuthentication.Strategy.Password.PasswordConfirmationValidation
 
       # Hashes the provided password
-      change(AshAuthentication.Strategy.Password.HashPasswordChange)
+      change AshAuthentication.Strategy.Password.HashPasswordChange
 
       # Generates an authentication token for the user
-      change(AshAuthentication.GenerateTokenChange)
+      change AshAuthentication.GenerateTokenChange
     end
 
     create :sign_in_with_magic_link do
-      description("Sign in or register a user with magic link.")
+      description "Sign in or register a user with magic link."
 
       argument :token, :string do
-        description("The token from the magic link that was sent to the user")
-        allow_nil?(false)
+        description "The token from the magic link that was sent to the user"
+        allow_nil? false
       end
 
-      upsert?(true)
-      upsert_identity(:unique_email)
-      upsert_fields([:email])
+      upsert? true
+      upsert_identity :unique_email
+      upsert_fields [:email]
 
       # Uses the information from the token to create or sign in the user
-      change(AshAuthentication.Strategy.MagicLink.SignInChange)
+      change AshAuthentication.Strategy.MagicLink.SignInChange
 
       metadata :token, :string do
-        allow_nil?(false)
+        allow_nil? false
       end
     end
 
     action :request_magic_link do
       argument :email, :ci_string do
-        allow_nil?(false)
+        allow_nil? false
       end
 
-      run(AshAuthentication.Strategy.MagicLink.Request)
+      run AshAuthentication.Strategy.MagicLink.Request
     end
   end
 
   policies do
     bypass AshAuthentication.Checks.AshAuthenticationInteraction do
-      authorize_if(always())
+      authorize_if always()
     end
 
     policy action([:register_with_password, :sign_in_with_password]) do
-      authorize_if(always())
+      authorize_if always()
     end
   end
 
   attributes do
-    uuid_primary_key(:id)
+    uuid_primary_key :id
 
     attribute :email, :ci_string do
-      allow_nil?(false)
-      public?(true)
+      allow_nil? false
+      public? true
     end
 
     attribute :hashed_password, :string do
-      sensitive?(true)
+      sensitive? true
     end
 
-    attribute(:confirmed_at, :utc_datetime_usec)
+    attribute :confirmed_at, :utc_datetime_usec
   end
 
   identities do
-    identity(:unique_email, [:email])
+    identity :unique_email, [:email]
   end
 end
