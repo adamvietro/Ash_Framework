446ab4013ff11fba13899b1b31888943662898c0
adam vietro <hunterafi@gmail.com>
2025-10-09 14:32:23 -0700

    Ablum Policies Set all the polcies for the Albums.

--- tunez/lib/tunez/music/album.ex
+++ tunez/lib/tunez/music/album.ex
@@ -3,100 +3,130 @@ defmodule Tunez.Music.Album do
     otp_app: :tunez,
     domain: Tunez.Music,
     data_layer: AshPostgres.DataLayer,
-    extensions: [AshGraphql.Resource, AshJsonApi.Resource]
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource],
+    authorizers: [Ash.Policy.Authorizer]
 
   graphql do
-    type :album
+    type(:album)
   end
 
   json_api do
-    type "album"
+    type("album")
   end
 
   postgres do
-    table "albums"
-    repo Tunez.Repo
+    table("albums")
+    repo(Tunez.Repo)
 
     references do
-      reference :artist, index?: true, on_delete: :delete
+      reference(:artist, index?: true, on_delete: :delete)
     end
   end
 
   actions do
-    defaults [:read, :destroy]
+    defaults([:read, :destroy])
 
     create :create do
-      accept [:name, :year_released, :cover_image_url, :artist_id]
+      accept([:name, :year_released, :cover_image_url, :artist_id])
     end
 
     update :update do
-      accept [:name, :year_released, :cover_image_url]
+      accept([:name, :year_released, :cover_image_url])
     end
   end
 
   validations do
-    validate numericality(:year_released,
-               greater_than: 1950,
-               less_than_or_equal_to: &__MODULE__.next_year/0
-             ),
-             where: [present(:year_released)],
-             message: "must be between 1950 and next year"
+    validate(
+      numericality(:year_released,
+        greater_than: 1950,
+        less_than_or_equal_to: &__MODULE__.next_year/0
+      ),
+      where: [present(:year_released)],
+      message: "must be between 1950 and next year"
+    )
 
-    validate match(
-               :cover_image_url,
-               ~r"^(https://|/images/).+(\.png|\.jpg)$"
-             ),
-             where: [changing(:cover_image_url)],
-             message: "must start with https:// or /images/"
+    validate(
+      match(
+        :cover_image_url,
+        ~r"^(https://|/images/).+(\.png|\.jpg)$"
+      ),
+      where: [changing(:cover_image_url)],
+      message: "must start with https:// or /images/"
+    )
   end
 
   attributes do
-    uuid_primary_key :id
+    uuid_primary_key(:id)
 
     attribute :name, :string do
-      allow_nil? false
-      public? true
+      allow_nil?(false)
+      public?(true)
     end
 
     attribute :year_released, :integer do
-      allow_nil? false
-      public? true
+      allow_nil?(false)
+      public?(true)
     end
 
     attribute :cover_image_url, :string do
-      public? true
+      public?(true)
     end
 
-    create_timestamp :inserted_at
-    update_timestamp :updated_at
+    create_timestamp(:inserted_at)
+    update_timestamp(:updated_at)
   end
 
   relationships do
     belongs_to :artist, Tunez.Music.Artist do
-      allow_nil? false
+      allow_nil?(false)
     end
 
-    belongs_to :created_by, Tunez.Accounts.User
-    belongs_to :updated_by, Tunez.Accounts.User
+    belongs_to(:created_by, Tunez.Accounts.User)
+    belongs_to(:updated_by, Tunez.Accounts.User)
   end
 
   calculations do
-    calculate :years_ago, :integer, expr(2025 - year_released)
+    calculate(:years_ago, :integer, expr(2025 - year_released))
 
-    calculate :string_years_ago,
-              :string,
-              expr("wow, this was released " <> years_ago <> " years ago!")
+    calculate(
+      :string_years_ago,
+      :string,
+      expr("wow, this was released " <> years_ago <> " years ago!")
+    )
   end
 
   def next_year, do: Date.utc_today().year + 1
 
   identities do
-    identity :unique_album_names_per_artist, [:artist_id, :name],
+    identity(:unique_album_names_per_artist, [:artist_id, :name],
       message: "already exists for this artist"
+    )
   end
 
   changes do
-    change relate_actor(:created_by, allow_nil?: true), on: [:create]
-    change relate_actor(:updated_by, allow_nil?: true)
+    change(relate_actor(:created_by, allow_nil?: true), on: [:create])
+    change(relate_actor(:updated_by, allow_nil?: true))
+  end
+
+  policies do
+    bypass actor_attribute_equals(:role, :admin) do
+      authorize_if(always())
+    end
+
+    policy action(:read) do
+      authorize_if(always())
+    end
+
+    policy action(:create) do
+      authorize_if(actor_attribute_equals(:role, :editor))
+    end
+
+    policy action(:update) do
+      authorize_if(relates_to_actor(:created_by))
+    end
+
+    policy action_type([:update, :destroy]) do
+      authorize_if(expr(^actor(:role) == :editor and created_by_id == ^actor(:id)))
+    end
   end
 end
