bade6ecaad3e7bb1bd9c96a8d5fe0d41f21367e6
adam vietro <hunterafi@gmail.com>
2025-10-09 12:03:31 -0700

    Set the API Polices Set the polices for the API so that we can sign-in and create an account as well as made sure that we send the token to the user so that they will be able to store it for later.

--- tunez/lib/tunez/accounts.ex
+++ tunez/lib/tunez/accounts.ex
@@ -2,9 +2,21 @@ defmodule Tunez.Accounts do
   use Ash.Domain, otp_app: :tunez, extensions: [AshJsonApi.Domain]
 
   json_api do
-    routes do
-      base_route "/users", Tunez.Accounts.User do
-        post(:register_with_password, route: "/register")
+    base_route "/users", Tunez.Accounts.User do
+      post :register_with_password do
+        route("/register")
+
+        metadata(fn _subject, user, _request ->
+          %{token: user.__metadata__.token}
+        end)
+      end
+
+      post :sign_in_with_password do
+        route("/sign_in")
+
+        metadata(fn _subject, user, _request ->
+          %{token: user.__metadata__.token}
+        end)
       end
     end
   end
