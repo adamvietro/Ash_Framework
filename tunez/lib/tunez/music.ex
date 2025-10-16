602d7ad2ac73cea31041a9605f074c5291bb4b0f
adam vietro <hunterafi@gmail.com>
2025-10-16 15:48:31 -0700

    First Nested Forms Commit We are going to add in a new resource for tracks to the album. Created the migration and then added in the track attributes.

--- tunez/lib/tunez/music.ex
+++ tunez/lib/tunez/music.ex
@@ -3,66 +3,67 @@ defmodule Tunez.Music do
 
   graphql do
     queries do
-      get(Tunez.Music.Artist, :get_artist_by_id, :read)
-      list(Tunez.Music.Artist, :search_artists, :search)
+      get Tunez.Music.Artist, :get_artist_by_id, :read
+      list Tunez.Music.Artist, :search_artists, :search
     end
 
     mutations do
-      create(Tunez.Music.Artist, :create_artist, :create)
-      update(Tunez.Music.Artist, :update_artist, :update)
-      destroy(Tunez.Music.Artist, :destroy_artist, :destroy)
+      create Tunez.Music.Artist, :create_artist, :create
+      update Tunez.Music.Artist, :update_artist, :update
+      destroy Tunez.Music.Artist, :destroy_artist, :destroy
 
-      create(Tunez.Music.Album, :create_album, :create)
-      update(Tunez.Music.Album, :update_album, :update)
-      destroy(Tunez.Music.Album, :destroy_album, :destroy)
+      create Tunez.Music.Album, :create_album, :create
+      update Tunez.Music.Album, :update_album, :update
+      destroy Tunez.Music.Album, :destroy_album, :destroy
     end
   end
 
   json_api do
     routes do
       base_route "/artists", Tunez.Music.Artist do
-        get(:read)
-        index(:search)
-        post(:create)
-        patch(:update)
-        delete(:destroy)
-        related(:albums, :read, primary?: true)
+        get :read
+        index :search
+        post :create
+        patch :update
+        delete :destroy
+        related :albums, :read, primary?: true
       end
 
       base_route "/albums", Tunez.Music.Album do
-        post(:create)
-        patch(:update)
-        delete(:destroy)
+        post :create
+        patch :update
+        delete :destroy
       end
     end
   end
 
   forms do
-    form(:create_album, args: [:artist_id])
+    form :create_album, args: [:artist_id]
   end
 
   resources do
     resource Tunez.Music.Artist do
-      define(:create_artist, action: :create)
-      define(:read_artists, action: :read)
-      define(:get_artist_by_id, action: :read, get_by: :id)
-      define(:update_artist, action: :update)
-      define(:destroy_artist, action: :destroy)
+      define :create_artist, action: :create
+      define :read_artists, action: :read
+      define :get_artist_by_id, action: :read, get_by: :id
+      define :update_artist, action: :update
+      define :destroy_artist, action: :destroy
 
-      define(:search_artists,
+      define :search_artists,
         action: :search,
         args: [:query],
         default_options: fn ->
           [load: [:album_count, :latest_album_year_released, :cover_image_url]]
         end
-      )
     end
 
     resource Tunez.Music.Album do
-      define(:create_album, action: :create)
-      define(:get_album_by_id, action: :read, get_by: :id)
-      define(:update_album, action: :update)
-      define(:destroy_album, action: :destroy)
+      define :create_album, action: :create
+      define :get_album_by_id, action: :read, get_by: :id
+      define :update_album, action: :update
+      define :destroy_album, action: :destroy
     end
+
+    resource Tunez.Music.Track
   end
 end
