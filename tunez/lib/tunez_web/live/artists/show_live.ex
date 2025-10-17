179dc3ea484f394c63120c162eab9b11b985517f
adam vietro <hunterafi@gmail.com>
2025-10-16 17:11:03 -0700

    Ordering Added in a new param for manage_relationship that will map the order the tracks will be added to the order of the track.

--- tunez/lib/tunez_web/live/artists/show_live.ex
+++ tunez/lib/tunez_web/live/artists/show_live.ex
@@ -10,7 +10,7 @@ defmodule TunezWeb.Artists.ShowLive do
   def handle_params(%{"id" => artist_id}, _url, socket) do
     artist =
       Tunez.Music.get_artist_by_id!(artist_id,
-        load: [:albums],
+        load: [albums: [:tracks]],
         actor: socket.assigns.current_user
       )
 
@@ -116,7 +116,7 @@ defmodule TunezWeb.Artists.ShowLive do
             </.button_link>
           </:action>
         </.header>
-        <.track_details tracks={[]} />
+        <.track_details tracks={@album.tracks} />
       </div>
     </div>
     """
