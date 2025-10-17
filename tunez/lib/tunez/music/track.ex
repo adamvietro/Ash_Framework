0b45e3e1bdcafc4c619710e979188160fe04bfc8
adam vietro <hunterafi@gmail.com>
2025-10-16 18:22:32 -0700

    API for Tracks First commit for the extend for the tracks to be within the API.

--- tunez/lib/tunez/music/track.ex
+++ tunez/lib/tunez/music/track.ex
@@ -3,70 +3,79 @@ defmodule Tunez.Music.Track do
     otp_app: :tunez,
     domain: Tunez.Music,
     data_layer: AshPostgres.DataLayer,
-    authorizers: [Ash.Policy.Authorizer]
+    authorizers: [Ash.Policy.Authorizer],
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource]
+
+  graphql do
+    type :track
+  end
+
+  json_api do
+    type "track"
+  end
+
+  postgres do
+    table "tracks"
+    repo Tunez.Repo
+
+    references do
+      reference :album, index?: true, on_delete: :delete
+    end
+  end
+
+  actions do
+    defaults [:read, :destroy]
+
+    create :create do
+      primary? true
+      accept [:order, :name, :album_id]
+      argument :duration, :string, allow_nil?: false
+      change Tunez.Music.Changes.MinutesToSeconds, only_when_valid?: true
+    end
+
+    update :update do
+      primary? true
+      accept [:order, :name]
+      require_atomic? false
+      argument :duration, :string, allow_nil?: false
+      change Tunez.Music.Changes.MinutesToSeconds, only_when_valid?: true
+    end
+  end
+
+  policies do
+    policy always() do
+      authorize_if accessing_from(Tunez.Music.Album, :tracks)
+      authorize_if action_type(:read)
+    end
+  end
 
   preparations do
     prepare build(load: [:number, :duration])
   end
 
-  policies do
-    policy always() do
-      authorize_if(accessing_from(Tunez.Music.Album, :tracks))
-      authorize_if(action_type(:read))
-    end
-  end
-
-  postgres do
-    table("tracks")
-    repo(Tunez.Repo)
-
-    references do
-      reference(:album, index?: true, on_delete: :delete)
-    end
-  end
-
   attributes do
-    uuid_primary_key(:id)
+    uuid_primary_key :id
 
     attribute :order, :integer do
-      allow_nil?(false)
+      allow_nil? false
     end
 
     attribute :name, :string do
-      allow_nil?(false)
+      allow_nil? false
     end
 
     attribute :duration_seconds, :integer do
-      allow_nil?(false)
-      constraints(min: 1)
+      allow_nil? false
+      constraints min: 1
     end
 
-    create_timestamp(:inserted_at)
-    update_timestamp(:updated_at)
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
   end
 
   relationships do
     belongs_to :album, Tunez.Music.Album do
-      allow_nil?(false)
-    end
-  end
-
-  actions do
-    defaults([:read, :destroy])
-
-    create :create do
-      primary?(true)
-      accept([:order, :name, :album_id])
-      argument(:duration, :string, allow_nil?: false)
-      change(Tunez.Music.Changes.MinutesToSeconds, only_when_valid?: true)
-    end
-
-    update :update do
-      primary?(true)
-      accept([:order, :name])
-      require_atomic? false
-      argument(:duration, :string, allow_nil?: false)
-      change(Tunez.Music.Changes.MinutesToSeconds, only_when_valid?: true)
+      allow_nil? false
     end
   end
 
