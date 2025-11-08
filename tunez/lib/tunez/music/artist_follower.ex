18309aa41836b38ed34636974ed8f4e223efec3e
adam vietro <hunterafi@gmail.com>
2025-11-07 16:25:38 -0800

    Policy Relationships Set the relationships and the policies so that we can now use the new DB. We also added in a default action to be able to read an artists followers.

--- tunez/lib/tunez/music/artist_follower.ex
+++ tunez/lib/tunez/music/artist_follower.ex
@@ -1,24 +1,39 @@
 defmodule Tunez.Music.ArtistFollower do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    authorizers: [Ash.Policy.Authorizer]
+
+  actions do
+    defaults([:read])
+  end
+
+  policies do
+    policy action_type(:read) do
+      authorize_if(always())
+    end
+  end
 
   postgres do
-    table "artist_followers"
-    repo Tunez.Repo
+    table("artist_followers")
+    repo(Tunez.Repo)
 
     references do
-      reference :artist, on_delete: :delete, index?: true
-      reference :follower, on_delete: :delete, index?: true
+      reference(:artist, on_delete: :delete, index?: true)
+      reference(:follower, on_delete: :delete, index?: true)
     end
   end
 
   relationships do
     belongs_to :artist, Tunez.Music.Artist do
-      primary_key? true
-      allow_nil? false
+      primary_key?(true)
+      allow_nil?(false)
     end
+
     belongs_to :follower, Tunez.Accounts.User do
-      primary_key? true
-      allow_nil? false
+      primary_key?(true)
+      allow_nil?(false)
     end
   end
 end
